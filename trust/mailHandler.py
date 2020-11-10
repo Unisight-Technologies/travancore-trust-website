7516772c3f37687ee27e0699c89ec3aa46e1e9d8
Naitik0909 <parmarnaitik0909@gmail.com>
2020-11-10 10:53:37 +0530

    Handle exceptions in mailHandler

--- trust/mailHandler.py
+++ trust/mailHandler.py
@@ -107,13 +107,16 @@ def sendMailToTravancoreIrregularDonation(zipcode, amount, type):
 def sendMailToContactPerson(name, send_to):
     subject = "Thanks for contacting us"
     message = "Hello "+name+"! \n\nWe have successfully received your message.\n\nWe will get back to you as soon as possible.\n\nRegards\n-The Travancore Charitable Trust"
-    send_mail(
-        subject,
-        message,
-        env("EMAIL"),
-        [send_to],
-        fail_silently = False,
-    )
+    try:
+        send_mail(
+            subject,
+            message,
+            env("EMAIL"),
+            [send_to],
+            fail_silently = False,
+        )
+    except:
+        print("EMAIL NOT SENT")
 
 
 def sendMailToTravancoreContact(name, email, message):
